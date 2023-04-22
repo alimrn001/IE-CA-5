b7051ceb09f3c966ac55ad7db5dee9e24be205de
mehrani <mehraniali8002@gmail.com>
2023-04-23 00:51:01 +0330

    add react-app plain subfolder

---
+++ front-end/react-app/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/login/login";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
