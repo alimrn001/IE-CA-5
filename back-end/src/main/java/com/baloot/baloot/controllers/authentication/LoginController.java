8e4d3c17e2a2e97008a7a51224c0e43a8d8967f3
mehrani <mehraniali8002@gmail.com>
2023-04-22 23:44:08 +0330

    fixed cors issue in spring

--- back-end/src/main/java/com/baloot/baloot/controllers/authentication/LoginController.java
+++ back-end/src/main/java/com/baloot/baloot/controllers/authentication/LoginController.java
@@ -7,18 +7,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Map;
 
 @RestController
+//@CrossOrigin("*")
 public class LoginController {
 
-    @GetMapping("/login") // must be actually @PostMapping
-    public String informLogin() {
-        try {
-            return LoginService.informLogin();
-        }
-        catch (Exception e) {
-            return e.getMessage();
-        }
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody Map<String, Object> payLoad) {
+        System.out.println("username and pass is : " + payLoad.get("username") + " - " + payLoad.get("password"));
+        return ResponseEntity.ok("ok");
     }
+
+//    @GetMapping("/login") // must be actually @PostMapping
+//    public String informLogin() {
+//        try {
+//            return LoginService.informLogin();
+//        }
+//        catch (Exception e) {
+//            return e.getMessage();
+//        }
+//    }
 
 }
--- back-end/src/main/java/com/baloot/baloot/filters/CORSFilter.java
+++ back-end/src/main/java/com/baloot/baloot/filters/CORSFilter.java
@@ -1,45 +1,63 @@
 package com.baloot.baloot.filters;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import org.springframework.core.Ordered;
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
+@Configuration
+public class CORSFilter {
 
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CORSFilter implements Filter {
-
-    @Override
-    public void init(FilterConfig fc) throws ServletException {
+    @Bean
+    public WebMvcConfigurer getCorsConfiguration(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
-
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        try {
-            Thread.sleep(200); // 0.2 seconds
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        HttpServletResponse response = (HttpServletResponse) resp;
-        HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
-        response.setHeader("Access-Control-Allow-Credentials", String.valueOf(true));
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            chain.doFilter(req, resp);
-        }
-    }
-
-    @Override
-    public void destroy() {
-    }
-
 }
+
+
+//import org.springframework.core.Ordered;
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//public class CORSFilter implements Filter {
+//
+//    @Override
+//    public void init(FilterConfig fc) throws ServletException {
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+//
+//        HttpServletResponse response = (HttpServletResponse) resp;
+//        HttpServletRequest request = (HttpServletRequest) req;
+//        response.setHeader("Access-Control-Allow-Origin", "*");
+//        response.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
+//        response.setHeader("Access-Control-Max-Age", "3600");
+//        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+//        response.setHeader("Access-Control-Allow-Credentials", String.valueOf(true));
+//        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+//            response.setStatus(HttpServletResponse.SC_OK);
+//        } else {
+//            chain.doFilter(req, resp);
+//        }
+//    }
+//
+//    @Override
+//    public void destroy() {
+//    }
+//
+//}
