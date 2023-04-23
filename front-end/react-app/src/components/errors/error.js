6b17da9a56a659fac3e9106f96709b23e0ac92d7
Mohammad Honarjou <honarjou79@gmail.com>
2023-04-23 13:20:51 +0330

    fixed some bugs in error pages

--- front-end/react-app/src/components/errors/error.js
+++ front-end/react-app/src/components/errors/error.js
@@ -11,37 +11,37 @@ class Error extends Component {
     return (
       <div>
         <Nav />
-        <div className="page-body">
+        <div className="page-body m-0">
             <div className="d-flex align-items-center justify-content-center vh-100">
                 <div className="text-center">
-                    <h1 className="display-1 fw-bold status-code">{this.props.errorCode}</h1>
+                    <h1 className="display-1 fw-bold status-code mb-0">{this.props.errorCode}</h1>
                     {
-                        this.props.errorCode == "404" &&
+                        this.props.errorCode === "404" &&
                         <p className="fs-3 status-msg">Page not found.</p>
                     }
                     {
-                        this.props.errorCode == "403" &&
+                        this.props.errorCode === "403" &&
                         <p className="fs-3 status-msg">Forbidden.</p>
                     }
                     {
-                        this.props.errorCode == "400" &&
+                        this.props.errorCode === "400" &&
                         <p className="fs-3 status-msg">Bad request.</p>
                     }
                     
                     {
-                        this.props.errorCode == "404" &&
+                        this.props.errorCode === "404" &&
                         <p className="lead status-msg-2">
                             The page you’re looking for doesn’t exist.
                         </p>
                     }
                     {
-                        this.props.errorCode == "403" &&
+                        this.props.errorCode === "403" &&
                         <p className="lead status-msg-2">
                             You dont have permission to access this page.
                         </p>
                     }
                     {
-                        this.props.errorCode == "400" &&
+                        this.props.errorCode === "400" &&
                         <p className="lead status-msg-2">
                             Your request resulted in an error.
                         </p>
